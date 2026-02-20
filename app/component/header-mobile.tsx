9717ea128e150452630f6ef7f3a81bd870c3685e
rdevelabs <rdevelabs@192.168.1.6>
2026-02-20 11:13:43 +0700

    update layout & style for desktop & mobile

--- app/component/header-mobile.tsx
+++ app/component/header-mobile.tsx
@@ -9,15 +9,15 @@ interface HeaderProps {
 
 export default function HeaderMobile({ search, setSearch }: HeaderProps) {
     return (
-        <div className="w-full h-max flex-col gap-1.5 flex text-black bg-amber-400">
-            <div className="h-max w-full  items-center justify-center p-2">
-                <h1 className="text-[23px] font-bold ">Daftar Seluruh Kontak</h1>
-                <p className="text-lg">Universitas Pelita Bangsa</p>
+        <div className="w-full h-max flex-col gap-1.5 flex text-black bg-(--bg-navbar)">
+            <div className="h-max w-full p-2">
+                <h1 className="text-[23px] font-bold text-(--text-navbar)">Daftar Seluruh Kontak</h1>
+                <p className="text-lg text-(--text-navbar)">Universitas Pelita Bangsa</p>
             </div>
             <div className="flex items-center justify-center mb-2">
                 <div className="relative">
-                    <FontAwesomeIcon className="absolute top-2 left-3 w-6" icon={faMagnifyingGlass}/>
-                    <input className="border-2 rounded-4xl pl-10 p-1 pr-3 text-sm w-full" type="text" id="cari" placeholder="Cari Nama Kontak" value={search} onChange={(e) => setSearch(e.target.value)}/>
+                    <FontAwesomeIcon className="absolute top-2 left-3 w-6 text-(--text-navbar)" icon={faMagnifyingGlass}/>
+                    <input className="border-2 border-black rounded-4xl pl-10 p-1 pr-3 text-sm w-full text-(--text-navbar) placeholder:text-(--text-navbar)" type="text" id="cari" placeholder="Cari Nama Kontak" value={search} onChange={(e) => setSearch(e.target.value)}/>
                 </div>
             </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
     const filterCallCenter = callCenter.filter((c) => c.nama.toLowerCase().includes(search.toLowerCase()));
   return (
     <div>
+      {/* desktop section */}
       <div className="hidden h-max md:flex flex-col items-end justify-center">
         <HeaderDesktop search={search} setSearch={setSearch} />
         <div className="body-card h-100 w-full rounded-tr-2xl rounded-bl-2xl p-4">
@@ -49,12 +50,18 @@ export default function Home() {
                         </tbody>
                       </table>
                     </div>
-                    <div className="flex items-center justify-center mr-10 gap-10">
-                      <a href={`https://mail.google.com/mail/?view=cm&fs=1&to=${d.email}`} target="_blank" rel="noopener noreferrer">
-                        <Image src="/gmail.svg" alt="gmail" width={40} height={40} />
+                    <div className="flex items-center justify-center gap-2">
+                      <a href={`mailto:${d.email}`} target="_blank" rel="noopener noreferrer">
+                        <div className="flex flex-col items-center justify-center gap-1 border-2 rounded-md p-2">
+                          <Image src="/gmail.svg" alt="gmail" width={40} height={40} />
+                          <p>Kirim Email</p>
+                        </div>
                       </a>
                       <a href={`https://wa.me/+62${d.telp}`} target="_blank" rel="noopener noreferrer">
-                        <Image src="/whatsapp.svg" alt="whatsapp" width={40} height={40} />
+                        <div className="flex flex-col items-center justify-center gap-1 border-2 rounded-md p-2">
+                          <Image src="/whatsapp.svg" alt="whatsapp" width={40} height={40} />
+                          <p>Kirim Pesan</p>
+                        </div>
                       </a>
                     </div>
                   </div>
@@ -73,11 +80,12 @@ export default function Home() {
                         </tbody>
                       </table>
                     </div>
-                    <div className="flex items-center justify-center mr-10 gap-10">
-                      <a href={`https://wa.me/+62${c.telp}`} target="_blank" rel="noopener noreferrer">
-                        <Image src="/whatsapp.svg" alt="whatsapp" width={40} height={40} />
-                      </a>
-                    </div>
+                    <a href={`https://wa.me/+62${c.telp}`} target="_blank" rel="noopener noreferrer">
+                      <div className="flex items-center justify-center gap-2 border-2 rounded-md p-2">
+                        <Image src="/whatsapp.svg" alt="whatsapp" width={25} height={25} />
+                        <p>Kirim Pesan</p>
+                      </div>
+                    </a>
                   </div>
                 ))}
           </div>
@@ -88,17 +96,17 @@ export default function Home() {
         </div>
       </div>
 
-
-      <div className="md:hidden flex flex-col w-screen">
+      {/* mobile section */}
+      <div className="md:hidden flex flex-col w-screen h-full">
         <div className="fixed w-full z-10">
             <HeaderMobile search={search} setSearch={setSearch} />
             <div className="flex w-full">
-              <button onClick={tabDosen} className={`w-full p-2   hover:bg-gray-200 ${menu === "dosen" ? "bg-gray-200" : ""}`}>Kontak Dosen</button>
-              <button onClick={tabCallCenter} className={`p-2 w-full  hover:bg-gray-200 ${menu === "callCenter" ? "bg-gray-200" : ""}`}>Call Center</button>
+              <button onClick={tabDosen} className={`w-full p-2 text-(--text-navbar) bg-(--navbar-menu) hover:bg-(--navbar-menu-hover) ${menu === "dosen" ? "bg-(--navbar-menu-hover)" : ""}`}>Kontak Dosen</button>
+              <button onClick={tabCallCenter} className={`p-2 w-full text-(--text-navbar) bg-(--navbar-menu) hover:bg-(--navbar-menu-hover) ${menu === "callCenter" ? "bg-(--navbar-menu-hover)" : ""}`}>Call Center</button>
             </div>
         </div>
-        <div className="w-full h-41 bg-blue"></div>
-        <div className="h-full body-card w-full">
+        <div className="w-full h-42 bg-blue"></div>
+        <div className="h-full w-full">
           <div className="flex flex-col items-center justify-start h-full overflow-y-scroll box-border">
             {menu === "dosen" && filterDosen.map((d) => (
                   <div key={d.id} className="flex flex-col gap-2 justify-between p-2 m-2 text-black bg-white rounded-lg shadow-md w-[calc(100%-1rem)] shrink-0">
@@ -141,22 +149,25 @@ export default function Home() {
                   </div>
                 ))}
               {menu === "callCenter" && filterCallCenter.map((c) => (
-                  <div key={c.id} className="flex items-center justify-between p-2 m-2 text-black bg-white rounded-lg shadow-md w-[calc(100%-1rem)] shrink-0">
-                    <div>
-                      <h2 className="text-xl font-bold">{c.nama}</h2>
+                  <div key={c.id} className="flex items-center gap-4 justify-between p-2 m-2 text-black bg-white rounded-lg shadow-md w-[calc(100%-1rem)] shrink-0">
+                    <div className="w-full">
+                      <h2 className="text-md font-bold">{c.nama}</h2>
                       <table>
                         <tbody>
                           <tr>
-                            <th className="text-left font-normal">Telepon</th>
-                            <th className="text-left font-normal pl-1 pr-1">:</th>
-                            <td className="text-sm">{c.telp}</td>
+                            <th className="text-[14px] text-left font-normal">Telepon</th>
+                            <th className="text-[14px] text-left font-normal pl-1 pr-1">:</th>
+                            <td className="text-[14px]">{c.telp}</td>
                           </tr>
                         </tbody>
                       </table>
                     </div>
-                    <div className="flex items-center justify-center mr-10 gap-10">
-                      <a href={`https://wa.me/+62${c.telp}`} target="_blank" rel="noopener noreferrer">
-                        <Image src="/whatsapp.svg" alt="whatsapp" width={40} height={40} />
+                    <div className="w-50">
+                      <a href={`https://wa.me/+62${c.telp}`} className="w-full" target="_blank" rel="noopener noreferrer">
+                        <div className="flex items-center justify-center gap-1 border-2 rounded-md p-1">
+                          <Image src="/whatsapp.svg" alt="whatsapp" width={17} height={17} />
+                          <p>Kirim Pesan</p>
+                        </div>
                       </a>
                     </div>
                   </div>
